6d99b3eae6813c5cba0cea0806d21b8fbeef0426
Jio Jang <talktalk0917@gmail.com>
2023-12-28 16:09:10 +0900

    first_commit_jio

--- app/src/main/java/com/example/madcamp_androidproject/MainActivity.kt
+++ app/src/main/java/com/example/madcamp_androidproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.madcamp_androidproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//hello
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

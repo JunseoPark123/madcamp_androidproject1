8223a6b60e23439322f379ad115f0d12f7451af6
Jio Jang <talktalk0917@gmail.com>
2023-12-29 22:35:17 +0900

    전화번호 추가 기능 구현 완료. 전화번호부 디자인 일부분 완료.

---
+++ app/src/main/java/com/example/madcamp_androidproject/itemFrameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.madcamp_androidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class itemFrameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_frame)
+    }
+}

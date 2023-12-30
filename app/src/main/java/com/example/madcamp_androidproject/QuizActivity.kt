9b33ac12ad135c75d2ee77fdd58b1bbed38e2ad3
JunseoPark123 <pjunseo7965@gmail.com>
2023-12-30 11:13:32 +0900

    tab3 작업 중간 저장

---
+++ app/src/main/java/com/example/madcamp_androidproject/QuizActivity.kt
@@ -0,0 +1,25 @@
+package com.example.madcamp_androidproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
+
+
+class QuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+
+        val btn_toeic : Button = findViewById(R.id.btn_toeic_myvoca)
+        btn_toeic.setOnClickListener {
+            val intent = Intent(this, QuizQuestionActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}

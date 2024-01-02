30edd5871a8b483f9a9e3b7be9e53491b9af07f7
Jio Jang <talktalk0917@gmail.com>
2024-01-02 11:10:34 +0900

    전화, 문자 기능 추가함

--- app/src/main/java/com/example/madcamp_androidproject/ContactAdapter.kt
+++ app/src/main/java/com/example/madcamp_androidproject/ContactAdapter.kt
@@ -3,12 +3,14 @@ package com.example.madcamp_androidproject
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -21,6 +23,8 @@ class ContactAdapter(private val contactList: List<Contact>, private val onConta
         val contactIcon: TextView = view.findViewById(R.id.contactIcon)
         val nameTextView: TextView = view.findViewById(R.id.textViewName)
         val phoneNumberTextView: TextView = view.findViewById(R.id.textViewPhoneNumber)
+        val callButton: ImageButton = view.findViewById<ImageButton>(R.id.call_button)
+        val messageButton: ImageButton = view.findViewById<ImageButton>(R.id.message_button)
 
         init {
             view.setOnClickListener {
@@ -46,6 +50,36 @@ class ContactAdapter(private val contactList: List<Contact>, private val onConta
         holder.contactIcon.text = contact.name.first().toString()
         holder.nameTextView.text = contact.name
         holder.phoneNumberTextView.text = contact.phoneNumber
+
+        // 전화 버튼에 클릭 리스너 설정
+        holder.callButton.setOnClickListener {
+            val context = holder.itemView.context
+            val phoneNumber = contact.phoneNumber
+
+            // Intent.ACTION_DIAL 사용하여 다이얼 패드 열기
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:$phoneNumber")
+            }
+            context.startActivity(intent)
+        }
+
+        // 메시지 버튼에 클릭 리스너 설정
+        holder.messageButton.setOnClickListener {
+            val context = holder.itemView.context
+            val phoneNumber = contact.phoneNumber
+
+            // Intent.ACTION_SENDTO 사용하여 SMS 앱 열기
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("smsto:$phoneNumber") // 여기에는 sms: 혹은 smsto: 를 사용할 수 있습니다.
+            }
+            // 인텐트에 사용 가능한 앱이 있는지 확인하고 있다면 앱을 시작합니다.
+            if (intent.resolveActivity(context.packageManager) != null) {
+                context.startActivity(intent)
+            } else {
+                Log.e("ContactAdapter", "No activity found to handle SMS intent")
+            }
+        }
+
         holder.itemView.setOnClickListener {
             onContactClicked(contact, position) // 클릭 이벤트에 리스너를 연결
         }

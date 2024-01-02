e92794444422acd22ca18b07234e46c8e1cde34e
Jio Jang <talktalk0917@gmail.com>
2024-01-02 16:36:34 +0900

    오류 수정. 정상작동

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.madcamp_androidproject"
-        minSdk = 26
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"

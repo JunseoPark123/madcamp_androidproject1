59ede856fdf60d9cdbe7d84355b7995ffa284136
Jio Jang <talktalk0917@gmail.com>
2024-01-03 11:17:13 +0900

    tab4 에서 푸쉬중 삭제시 창 뜨도록 만듬

--- app/src/main/java/com/example/madcamp_androidproject/DetailContactActivity.kt
+++ app/src/main/java/com/example/madcamp_androidproject/DetailContactActivity.kt
@@ -3,11 +3,13 @@ package com.example.madcamp_androidproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class DetailContactActivity : AppCompatActivity() {
     private lateinit var contactInitialTextView: TextView
@@ -16,6 +18,9 @@ class DetailContactActivity : AppCompatActivity() {
     private lateinit var saveButton: ImageButton
     private lateinit var deleteButton: ImageButton
     private lateinit var goBackButton: ImageButton
+    private lateinit var deleteConfirmationCardView: CardView
+    private lateinit var darkOverlay: View
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +33,10 @@ class DetailContactActivity : AppCompatActivity() {
         saveButton = findViewById<ImageButton>(R.id.saveButton)
         deleteButton = findViewById<ImageButton>(R.id.delete_button)
         goBackButton = findViewById<ImageButton>(R.id.GoBack_button)
+        // 삭제 확인 및 오버레이 뷰 초기화
+        darkOverlay = findViewById(R.id.darkOverlay)
+        deleteConfirmationCardView = findViewById(R.id.deleteConfirmationCardView)
+
 
 
         // Intent에서 연락처 정보 가져오기
@@ -62,12 +71,34 @@ class DetailContactActivity : AppCompatActivity() {
             }
         }
 
+        // 삭제 버튼 클릭 리스너
         deleteButton.setOnClickListener {
-            // 삭제할 연락처의 정보를 결과로 설정하고 액티비티를 종료합니다.
-            val resultIntent = Intent()
-            resultIntent.putExtra("DELETED_CONTACT", contact)
-            setResult(RESULT_FIRST_USER, resultIntent) // RESULT_OK 대신 다른 결과 코드를 사용합니다.
+            // 오버레이 및 삭제 확인 카드뷰 표시
+            darkOverlay.visibility = View.VISIBLE
+            deleteConfirmationCardView.visibility = View.VISIBLE
+        }
+
+
+        // "확인" 버튼 클릭 리스너 설정
+        val buttonConfirmDelete: Button = findViewById(R.id.buttonConfirmDelete)
+        buttonConfirmDelete.setOnClickListener {
+            // 연락처 삭제 로직을 여기에 추가합니다.
+            // 예를 들어, 삭제 결과를 메인 액티비티에 전달합니다.
+            val resultIntent = Intent().apply {
+                putExtra("DELETED_CONTACT", contact)
+                setResult(RESULT_FIRST_USER, this)
+            }
             finish()
+            darkOverlay.visibility = View.GONE
+            deleteConfirmationCardView.visibility = View.GONE
+        }
+
+        // "취소" 버튼 클릭 리스너 설정
+        val buttonCancelDelete: Button = findViewById(R.id.buttonCancelDelete)
+        buttonCancelDelete.setOnClickListener {
+            // CardView를 숨깁니다.
+            darkOverlay.visibility = View.GONE
+            deleteConfirmationCardView.visibility = View.GONE
         }
 
         goBackButton.setOnClickListener {
